e60a9333670106b5fcc7204ac436bda6680d7852
Abmin <pavlo.koshubinskyi@oa.edu.ua>
2021-05-20 15:53:36 +0300

    lab 10

--- app/Repositories/BlogCategoryRepository.php
+++ app/Repositories/BlogCategoryRepository.php
@@ -47,6 +47,7 @@ class BlogCategoryRepository extends CoreRepository
         $result = $this                           //2 варіант
         ->startConditions()
             ->selectRaw($columns)
+
             ->toBase()
             ->get();
 
@@ -68,6 +69,7 @@ class BlogCategoryRepository extends CoreRepository
         $result = $this
             ->startConditions()
             ->select($columns)
+            ->with(['parentCategory:id,title',])
             ->paginate($perPage); //можна $columns додати сюди
 
         return $result;
